2d17fffbca341af93a81b531dc410880a10b0c9f
Chairoj/Sinlapachai <40155594+Sin1apachai@users.noreply.github.com>
2023-10-30 16:55:26 +0700

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -60,7 +60,8 @@ DirectoryIndex index.php index.html index.htm
         setsebool -P domain_can_mmap_files on
         setsebool -P httpd_unified on
         mysql -u root -e "CREATE DATABASE $user_db;"
-        mysql -u root -e "grant all privileges on $user_db.* to '$user_db'@'localhost' identified by '$password_db';"
+        mysql -u root -e "grant all privileges on $user_db.* to '$user_db'@'localhost';"
+        mysql -u root -e "ALTER USER '$user_db'@'localhost' IDENTIFIED BY '$password_db';"
         mysql -u root -e "flush privileges;"
         wget https://wordpress.org/latest.tar.gz -O /var/www/latest.tar.gz
         tar zxvf latest.tar.gz -C /var/www/
